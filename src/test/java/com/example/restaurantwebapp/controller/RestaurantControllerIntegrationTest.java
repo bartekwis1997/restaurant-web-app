5eece61cf21b22c9cb0692def93979eb968d2596
Bartek <bartekwis97@gmail.com>
2024-05-12 19:05:06 +0200

    Small test fix

--- src/test/java/com/example/restaurantwebapp/controller/RestaurantControllerIntegrationTest.java
+++ src/test/java/com/example/restaurantwebapp/controller/RestaurantControllerIntegrationTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -24,7 +23,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(RestaurantController.class)
-@AutoConfigureMockMvc
 class RestaurantControllerIntegrationTest {
 
     @Autowired
